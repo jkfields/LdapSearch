e646918e0fd8d1d051baed666539d5f61348dfec
Jeff Fields <jkfields@yahoo.com>
2021-01-30 07:53:03 -0500

    Update ldapsearch.py

--- ldapsearch.py
+++ ldapsearch.py
@@ -1,27 +1,40 @@
-#!/usr/bin/env python
+"""
+"""
 
 import platform
 from subprocess import Popen, PIPE
 
 # TODO:  push these to config file
-LINUX_CMD = ""
-SOL_CMD = ""
+LINUX_CMD = "/bin/ldapsearch -LLL -o ldif-wrap=no -xH {url} -b {dn} {filter} {attrs}"
+SOL_CMD = "/usr/bin/ldapsearch -T -x -h {url} -b {dn} {filter} {attrs}"
 
 class InvalidLdapSearchCommandException(Exception):
     pass
     
-
 class LdapSearch:
     def __init__(self, url):
         self.__url = url
+        self.__system = platform.system().lower() 
         
-        if platform.system().lower() == "linux":
-            self.__cmd = LINUX_CMD
-        else
+        if self.__system == "sunos":
             self.__cmd = SOL_CMD
+        else if
+            self.__cmd = LINUX_CMD
             
-    def search(self, basedn, filter, attrs):
-        pass
+    def search(self, dn, filter, attrs):
+        __cmd = self.__cmd.format(url=self.__url,
+                                  dn=dn,
+                                  filter=filter,
+                                  attrs=attrs)
+        __proc = Popen(__cmd.split(), shell=False, stdout=PIPE, stderr=PIPE)
+        __out, __err = __proc.communicate()
+        __rtncode = __proc.returncode
+        
+        if __rtncode == 0:
+            return __outo
+        else:
+            raise InvalidLdapSeachCommandException("{rtncode}::{msg}".format(rtn_code=__rtncode,
+                                                                             msg= __err)
         
     @static
     def dtToIso(dt):
