bb94fbfcd0a07662dd891151c80698b5efb5b49f
Jeff Fields <jkfields@yahoo.com>
2022-07-21 15:11:08 -0400

    Update ldapsearch.py

--- ldapsearch.py
+++ ldapsearch.py
@@ -6,33 +6,83 @@ import platform
 from subprocess import Popen, PIPE
 
 # TODO:  push these to config file
-LINUX_CMD = "/bin/ldapsearch -LLL -o ldif-wrap=no -xH {url} -b {dn} {filter} {attrs}"
-SOL_CMD = "/usr/bin/ldapsearch -T -1 -h {url} -b {dn} {filter} {attrs}"
+DEFAULT_HOST = "ldap1"
+DEFAULT_PORT = 389
+DEFAULT_BASEDN = "ou=people,ou=systems,dc=dc1,dc=dc2,dc=dc3,dc=dc4"
+DEFAULT_FILTER = "(objectClass=person)"
+DEFAULT_ATTRS = "sn givenName middleName uid uidNumber gid gidNumber telephoneNumber email"
 
-class InvalidLdapSearchCommandException(Exception):
+LINUX_CMD = "/bin/ldapsearch -LLL -o ldif-wrap=no -xH {url} -b {dn} {filter} {attrs}"
+SOL_CMD = "/usr/bin/ldapsearch -T1h {url} -b {dn} {filter} {attrs}"
+
+
+class LdapSearchError(Exception):
+    """Raised when an error occues executing the ldapsearch"""
     pass
     
+
+class ShellExecutionError(Exception):
+    """Raised when an error occurs executing a shell command"
+    pass
+
+
 class LdapSearch:
-    def __init__(self, url):
-        self.__url = url
-        self.__system = platform.system().lower() 
+    system = platform.system().lower()
+
+
+    def __init__(self, host, port=389):
+        self.host = host
+        self.port = port 
         
-        if self.__system == "sunos":
-            self.__cmd = SOL_CMD
-        else if
-            self.__cmd = LINUX_CMD
-            
-    def search(self, dn, filter, attrs):
-        __cmd = self.__cmd.format(url=self.__url,
-                                  dn=dn,
-                                  filter=filter,
-                                  attrs=attrs)
-        __proc = Popen(__cmd.split(), shell=False, stdout=PIPE, stderr=PIPE)
-        __out, __err = __proc.communicate()
-        __rtncode = __proc.returncode
-        
-        if __rtncode == 0:
-            return __out
+        if self.system == "sunos":
+            self.ldapssearch = SOL_CMD
+        elif
+            self.ldapsearch = LINUX_CMD
         else:
-            raise InvalidLdapSeachCommandException("{rtncode}::{msg}".format(rtn_code=__rtncode,
-                                                                             msg= __err)
+            msg = "{0} is not a supported system".fort(self.system)
+            raise LdapSearchError(msg)
+
+
+    def __str__(self):
+        # TODO
+        pass
+
+
+    def search(self, dn=DEFAULT_BASEDN, filter=DEFAULT_FILTER, attrs=DEFAULT_ATTRS):
+       if self.system == "sunos":
+           cmd = self.ldapsearch.format(host=self.host,
+                                        dn=dn,
+                                        filter=filter,
+                                        attrs=attrs)
+        else:
+            cmd = self.ldapsearch.format(uri="ldap:\\{0}:{1}".format(self.host, self.port),
+                                         dn=dn,
+                                         filter=filter,
+                                         attrs=attrs)
+        self.ldif = LdapSearch.shex(cmd)
+
+
+    @staticmethod
+    def shex(cmd):
+        try:
+            proc = Popen(__cmd.split(), shell=False, stdout=PIPE, stderr=PIPE)
+            out, err = proc.communicate()
+            rtncode = proc.returncode
+            
+        except  Exception as ex:
+            out = ""
+            msg = "Execution failed; return code: {0}.   {1}.format(rtncode, str(ex))
+            raise ShellExecutionError(msg)
+            
+        else:
+            if __rtncode == 0:
+                return out
+            else:
+                msg = "Execution failed; return code: {0}.   {1}.format(rtncode, err)
+                raise ShellExecutionError(msg)
+
+if __name__ == "__main__":
+    ls = LdapSearch(HOST, PORT)
+    ls.search(DEFAULT_BASEDN, DEFAULT_FILTER, DEFAULT_ATTRS)
+    
+    print(ls.ldif)
